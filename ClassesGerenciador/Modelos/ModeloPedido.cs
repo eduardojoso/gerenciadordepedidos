c3dd6c1c5a1d36e212919a914b28a84e11d66d93
Lucas Silva Gomes <gomeslucassilva94@gmail.com>
2024-05-28 21:12:15 -0300

    Pedidos Inserir e Editar

--- ClassesGerenciador/Modelos/ModeloPedido.cs
+++ ClassesGerenciador/Modelos/ModeloPedido.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ClassesGerenciador.Modelos
@@ -11,8 +13,24 @@ namespace ClassesGerenciador.Modelos
         public int Id { get; set; }
         public int IdUser { get; set; }
         public string Descricao { get; set; }
+        public decimal Valor { get; set; }
         public string Cliente { get; set; }
-        public bool Status { get; set; }
+        public string Status { get; set; }
+        public DateOnly Data { get; set; }
 
+
+
+        public void InserirPedido(int idUser, string descricao, decimal valor, string cliente, string status, DateOnly data) 
+        {
+            string dateOnly = data.ToString("yyyy-MM-dd");
+            DBConnection connection = new DBConnection();
+            connection.DbConnection($"INSERT INTO Pedidos (id_Usuario, Descricao, Valor, Cliente, Status, Data) VALUES ('{idUser}', '{descricao}', '{valor}', '{cliente}', '{status}', '{dateOnly}');");
+        }
+
+        public void EditarPedido(int id, int idUser, string descricao, decimal valor, string cliente, string status) 
+        {
+            DBConnection connection = new DBConnection();
+            connection.DbConnection($"UPDATE Pedidos SET id_Usuario='{idUser}', Descricao='{descricao}', Valor='{valor}', Cliente='{cliente}', Status='{status}' WHERE idPedidos={id};");
+        }
     }
 }
---
+++ GerenciadorDePedidos/Controllers/PedidosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClassesGerenciador.Modelos;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GerenciadorDePedidos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+        // GET: api/<PedidosController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PedidosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PedidosController>
+        [HttpPost]
+        public void Post([FromBody] ModeloPedido pedido)
+        {
+            pedido.InserirPedido(pedido.IdUser, pedido.Descricao, pedido.Valor, pedido.Cliente, pedido.Status, pedido.Data);
+        }
+
+        // PUT api/<PedidosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ModeloPedido pedido)
+        {
+            pedido.EditarPedido(id, pedido.IdUser, pedido.Descricao, pedido.Valor, pedido.Cliente, pedido.Status);
+        }
+
+    }
+}
